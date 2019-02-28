4a3e069200d4c73e13ff49342b463cd23c008bb3
ChongqingWangYu <1903372529@qq.com>
2019-02-28 21:58:39 +0800

    打开转盘锁(BFS），思路不清晰，查阅资料勉强通过。

--- src/算法题库/打开转盘锁.java
+++ src/算法题库/打开转盘锁.java
@@ -127,5 +127,89 @@ public class 打开转盘锁 {
         }
         return list;
     }
+
+    /**
+     * LeetCode最优解，大神的代码。
+     * @param deadends
+     * @param target
+     * @return
+     */
+    public int openLock1(String[] deadends, String target) {
+        boolean[] isVisit=new boolean[10000];
+        boolean[] isDead=new boolean[10000];
+        for (String deadEnd : deadends) {
+            int deadNum = Integer.parseInt(deadEnd);
+            isDead[deadNum] = true;
+        }
+        //如果"0000"在deadEnds中，返回-1
+        if(isDead[0]) {
+            return -1;
+        }
+        if("0000".equals(target)) {
+            return 0;
+        }
+
+        int step=0;
+
+
+        Queue<Integer> queue=new ArrayDeque<>();
+        queue.offer(Integer.parseInt(target));
+        isVisit[Integer.parseInt(target)]=true;
+        int last=Integer.parseInt(target);
+        int cenglast=last;
+        while(!queue.isEmpty())
+        {
+            while(true)
+            {
+                int[] neighbor=new int[8];
+                int head=queue.poll();
+                if(head==0) {
+                    return step;
+                }
+                int[] nei=neighbor(head);
+                for(int i=0;i<8;i++)
+                {
+                    int trans=nei[i];
+                    if(isVisit[trans] || isDead[trans]) {
+                        continue;
+                    }
+                    queue.offer(trans);
+                    last=trans;
+                    isVisit[trans]=true;
+                }
+                if(head==cenglast) {
+                    break;
+                }
+            }
+            step++;
+            cenglast=last;
+        }
+        return -1;
+
+
+    }
+
+    /**
+     * LeetCode最优解，大神的代码。
+     * @param code
+     * @return
+     */
+    private int[] neighbor(int code)
+    {
+        int[] res=new int[8];
+        int a=code%10;
+        int b=(code/10)%10;
+        int c=(code/100)%10;
+        int d=(code/1000)%10;
+        res[0]=d*1000+c*100+b*10+(a+10-1)%10;
+        res[1]=d*1000+c*100+b*10+(a+1)%10;
+        res[2]=d*1000+c*100+((b+10-1)%10)*10+a;
+        res[3]=d*1000+c*100+((b+1)%10)*10+a;
+        res[4]=d*1000+((c+10-1)%10)*100+b*10+a;
+        res[5]=d*1000+((c+1)%10)*100+b*10+a;
+        res[6]=((d+10-1)%10)*1000+c*100+b*10+a;
+        res[7]=((d+1)%10)*1000+c*100+b*10+a;
+        return res;
+    }
 }
 
