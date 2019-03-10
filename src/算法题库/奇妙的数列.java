6d00be67847cfc20d182b0adc4f3aa86670682c7
ChongqingWangYu <1903372529@qq.com>
2019-03-10 09:57:54 +0800

    日常刷题(26)

---
+++ src/算法题库/奇妙的数列.java
@@ -0,0 +1,53 @@
+package 算法题库;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName 奇妙的数列
+ * @Description 腾讯笔试题
+ * @Author ChongqingWangYu
+ * @DateTime 2019/3/9 19:51
+ * @GitHub https://github.com/ChongqingWangYu
+ */
+public class 奇妙的数列 {
+    //-1,2,-3,4,-5 求和2秒内https://www.nowcoder.com/discuss/160361?tdsourcetag=s_pctim_aiomsg
+
+    /**
+     * 4
+     * 2 4 正数 正数 3
+     * 2 2 正数 正数 2
+     * 3 3 负数 负数 -3
+     * 1 5 负数 负数 -3
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //询问q次
+        int q = sc.nextInt();
+        while (q-- > 0) {
+            //左端点
+            int l = sc.nextInt();
+            //右端点
+            int r = sc.nextInt();
+            //区间长度（3-6+1）=4（3456）
+            int d = r - l + 1;
+            //奇数区间长度
+            if (d % 2 == 1) {
+                //奇数结束
+                if (r % 2 == 1) {
+                    System.out.println(d / 2 - r);
+                } else {//偶数结束
+                    System.out.println(r - d / 2);
+                }
+            } else {//偶数区间长度
+                //奇数结束
+                if (r % 2 == 1) {
+                    System.out.println(-d / 2);
+                } else {//偶数结束
+                    System.out.println(d / 2);
+                }
+            }
+        }
+    }
+}
---
+++ src/算法题库/硬币.java
@@ -0,0 +1,29 @@
+package 算法题库;
+
+import java.util.Scanner;
+
+/**
+ * @ClassName 硬币
+ * @Description 腾讯笔试题
+ * @Author ChongqingWangYu
+ * @DateTime 2019/3/9 19:33
+ * @GitHub https://github.com/ChongqingWangYu
+ */
+public class 硬币 {
+
+    /**
+     * 拥有1~N面值的硬币，例如N为100：1，2，3，4，5，6，7，8......100。
+     * 商品价格M小于N，则结果为1
+     * M大于N，结果为(n+m-1)/n 例如：N=10，M=500，509/10=50.9，int型50.9=50
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //硬币最大面额（面额为1到n）
+        int n = sc.nextInt();
+        //商品价格
+        int m = sc.nextInt();
+        System.out.println((n + m - 1) / n);
+    }
+}
