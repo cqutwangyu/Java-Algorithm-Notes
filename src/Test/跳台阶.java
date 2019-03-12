2c0a2effb36fa65e0ab0c72e53e2fe0080dc39fb
ChongqingWangYu <1903372529@qq.com>
2019-03-12 22:41:34 +0800

    日常刷题(31)

--- src/Test/跳台阶.java
+++ src/Test/跳台阶.java
@@ -1,7 +1,5 @@
 package Test;
 
-import java.util.jar.JarEntry;
-
 /**
  * @ClassName 跳台阶
  * @Description
@@ -11,17 +9,60 @@ import java.util.jar.JarEntry;
  */
 public class 跳台阶 {
     public static void main(String[] args) {
-        System.out.println(jumpFloor(20));
-
+        System.out.println(JumpFloor(10));
     }
 
-    public static int jumpFloor(int target) {
+    /**
+     * 跳台阶
+     *
+     * @param n
+     * @return
+     */
+    private static int JumpFloor(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        if (n <= 2) {
+            return n;
+        }
+        return JumpFloor(n - 1) + JumpFloor(n - 2);
+    }
+
+    /**
+     * 跳台阶 动态规则实现
+     *
+     * @param n
+     * @return
+     */
+    private static int JumpFloorDp(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        int f = 1;
+        int g = 1;
+        while (n-- > 0) {
+            g = g + f;
+            f = g - f;
+        }
+        return f;
+    }
+
+    /**
+     * 变态跳台阶
+     * 左移1位就是乘以一个2，在1基础上左移number-1位就是1乘以2^(number-1)
+     *
+     * @param target
+     * @return
+     */
+    public static int JumpFloorII(int target) {
+        //究极解法，用位移
+//        return 1<<(target-1);
         if (target == 0) {
             return -1;
         }
         if (target == 1) {
             return 1;
         }
-        return 2 * jumpFloor(target - 1);
+        return 2 * JumpFloorII(target - 1);
     }
 }
